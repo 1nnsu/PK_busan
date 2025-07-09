894252d6459df3c39955474114f14a2be8129589
enuu <enuu9923@gmail.com>
2025-07-09 17:17:38 +0900

    수정중

--- script/script.js
+++ script/script.js
@@ -1,7 +1,29 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+    b_menu_hide();
+    close_btn();
 })
 
 
+const b_menu_hide = () => {
+    window.addEventListener("scroll", function () {
+    const scrollTop = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+    const side_menu = document.querySelector(".side_menu");
+
+    if (scrollTop + windowHeight >= documentHeight) {
+        side_menu.classList.add("hide"); // 스크롤 끝 → 숨김
+    } else {
+      side_menu.classList.remove("hide"); // 다시 올라오면 보여줌
+    }
+    });
+}
+const close_btn = () => {
+    $('.close_btn').click(function(){
+        $('.list_1').toggle('dn')
+        $(this).find('.close_icon').slideToggle();
+        $(this).find('.open_icon').slideToggle();
+    })
+}
